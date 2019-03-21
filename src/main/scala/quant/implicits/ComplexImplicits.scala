d5286dfd58688c91a765a9bb70b4a4bdb79429de
Sergei Dovzhikov <illidanboss@gmail.com>
2019-03-21 11:51:37 +0300

    tuned formating of complex numbers

--- src/main/scala/quant/implicits/ComplexImplicits.scala
+++ src/main/scala/quant/implicits/ComplexImplicits.scala
@@ -7,25 +7,45 @@ trait ComplexImplicits {
 
   implicit class RichComplex(c: Complex) {
     def prettyPrint: String =
-      if (isClose(c.imag.abs, 0.0))
-        f"${c.real}%.3f".replace(',','.')
-      else if (isClose(c.real.abs, 0.0))
+      if (isClose(c.imag, 0.0))
+        if (c.real.isValidInt)
+          c.real.toInt.toString
+        else
+          f"${c.real}%.3f".replaceAll("0+$", "").replace(',','.')
+      else if (isClose(c.real, 0.0))
         if (isClose(c.imag, 1.0))
           "i"
         else if (isClose(c.imag, -1.0))
           "-i"
+        else {
+          val is =
+            if (c.imag.isValidInt) c.imag.toInt.toString
+            else f"${c.imag}%.3f".replaceAll("0+$", "").replace(',','.')
+          s"$is*i"
+        }
+      else {
+        val rs =
+          if (c.real.isValidInt) c.real.toInt.toString
+          else f"${c.real}%.3f".replaceAll("0+$", "").replace(',','.')
+        if (c.imag < 0.0)
+          if (isClose(c.imag, -1.0))
+            s"$rs-i"
+          else {
+            val is =
+              if (c.imag.isValidInt) c.imag.toInt.toString
+              else f"${c.imag.abs}%.3f".replaceAll("0+$", "").replace(',', '.')
+            s"$rs-$is*i"
+          }
         else
-          f"${c.imag}%.3f*i".replace(',','.')
-      else if (c.imag < 0.0)
-        if (isClose(c.imag, -1.0))
-          f"${c.real}%.3f-i".replace(',','.')
-        else
-          f"${c.real}%.3f-${c.imag.abs}%.3f*i".replace(',','.')
-      else
-      if (isClose(c.imag, 1.0))
-        f"${c.real}%.3f+i".replace(',','.')
-      else
-        f"${c.real}%.3f+${c.imag}%.3f*i".replace(',','.')
+          if (isClose(c.imag, 1.0))
+            s"$rs+i"
+          else {
+            val is =
+              if (c.imag.isValidInt) c.imag.toInt.toString
+              else f"${c.imag}%.3f".replaceAll("0+$", "").replace(',', '.')
+            s"$rs+$is*i"
+          }
+      }
   }
 
 }
